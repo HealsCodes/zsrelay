e91c8eab7628c53bf1c4bd4652cf17644f24343d
René Köcher <shirk@bitspin.org>
2008-08-06 19:54:47 +0200

    Updated license notes to GPLv3

--- zsrelay.h
+++ zsrelay.h
@@ -1,37 +1,25 @@
 /*
-  zsrelay.h:
-  $Id: zsrelay.h,v 1.14 2003/04/14 05:36:15 bulkstream Exp $
-         common definitions.
+   This file is part of zsrelay a srelay port for the iPhone.
 
-Copyright (C) 2001 Tomo.M (author).
-All rights reserved.
+   Copyright (C) 2008 Rene Koecher <shirk@bitspinn.org>
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
+   Based on srelay 0.4.6 source base (C) 2001 Tomo.M
+   Destributed under the GPL license with original authors permission.
 
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. Neither the name of the author nor the names of its contributors
-   may be used to endorse or promote products derived from this software
-   without specific prior written permission.
+   zsrelay is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or (at
+   your option) any later version.
 
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -43,9 +31,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/time.h>
 #include <string.h>
 #include <errno.h>
-#define _POSIX_C_SOURCE
-#include <sys/signal.h>
-#undef  _POSIX_C_SOURCE
 #include <signal.h>
 
 #if HAVE_CONFIG_H
@@ -105,8 +90,8 @@ typedef    u_int32_t    socklen_t;
 # endif
 #endif
 
-#define version  "XSRelay 0.4.6.1 (C) 2008 (Rene.K)\n" \
-		 "based on zsrelay 0.4.6 (C) 2003/04/13 (Tomo.M)"
+#define version  "zselay 0.5.0 (C) 2008 (Rene.K)\n" \
+		 "based on srelay 0.4.6 (C) 2003/04/13 (Tomo.M)"
 
 #ifndef SYSCONFDIR
 # define SYSCONFDIR "/usr/local/etc"
